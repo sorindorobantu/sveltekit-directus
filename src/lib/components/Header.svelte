f2ff0f8895bf29644e7146b02af28dd43635bda5
sorindorobantu <sorin.cristian.dorobantu@gmail.com>
2022-05-27 07:37:51 +0200

    3 files modified, 3 added

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script> 
+console.log('h');
+
+</script>
+
+<header>
+h
+</header>
+
+<style>
+  header{
+    background-color: var(--header-background, #e1e1e1)
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,10 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/components/Header.svelte'
 </script>
 
 
-
+<Header/>
 <main>
 	<slot />
 </main>
